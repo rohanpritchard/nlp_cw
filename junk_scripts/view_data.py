760a11a79da26754bfc7a38976426a088fe099d9
Rohan Pritchard <rohanpritchard@icloud.com>
2020-02-10 18:36:50 +0000

    Turn view_data into get_data.

--- junk_scripts/view_data.py
+++
@@ -1,9 +0,0 @@
-def print_data(set):
-    source = "../en-de/%s.ende.src" % set
-    mt = "../en-de/%s.ende.mt" % set
-    scores = "../en-de/%s.ende.scores" % set
-    with open(source, "rb") as source, open(mt, "rb") as mt, open(scores, "rb") as scores:
-        for a, b, c in zip(source.readlines(), mt.readlines(), scores.readlines()):
-            print(a, b, c)
-
-print_data("dev")
