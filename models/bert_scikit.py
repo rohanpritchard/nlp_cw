e21cda85876d69b54e7ea0004e0671b6f179fc82
Rohan Pritchard <rohanpritchard@icloud.com>
2020-02-24 19:55:22 +0000

    Check scikit model with val data.

--- models/bert_scikit.py
+++ models/bert_scikit.py
@@ -7,16 +7,22 @@ from scipy.stats.stats import pearsonr
 print("Getting data...")
 with open("bert_encoded", "rb") as f:
     data = pickle.load(f)
+with open("bert_encoded_val", "rb") as f:
+    data1 = pickle.load(f)
 print("Tokenized data")
 
 data_averaged = []
 for e, c, l in data:
     data_averaged.append((np.array(list(e[0])+list(c[0])), l))
 
+data_averaged1 = []
+for e, c, l in data1:
+    data_averaged1.append((np.array(list(e[0])+list(c[0])), l))
+
 print("Train!")
 model = nn.MLPRegressor(hidden_layer_sizes=(500,), verbose=True)
-train = data_averaged[:-1000]
-test = data_averaged[-1000:]
+train = data_averaged
+test = data_averaged1
 X = [x[0] for x in train]
 y = [x[1] for x in train]
 X_test = [x[0] for x in test]
